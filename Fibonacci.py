1cf7d6ab1271380cb79250924414c44f2d33758c
huny <hy546880109@qq.com>
2020-11-08 14:05:11 +0800

    新增递归方法实现斐波那契数列

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,4 +1,4 @@
-#斐波那契数列
+#斐波那契数列 0、1、1、2、3、5、8、13、21、34
 list = [0,1]
 max = input('输入最大位数:')	#限制循环次数
 if max.isdigit():	#判断输入的是否为数字
@@ -8,3 +8,18 @@ if max.isdigit():	#判断输入的是否为数字
 else:
 	print('你输入的不是数字')
 
+
+#递归法
+# 斐波那契数列 0、1、1、2、3、5、8、13、21、34
+new_list = []
+n = int(input('please input number:'))
+def fun(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1 
+    else:          
+        return fun(n-1) + fun(n-2)
+for i in range(1,n+1):
+    new_list.append(fun(n))
+print(new_list)
